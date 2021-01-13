24948a9134dfaf7c84fe19ec9af0ac6137f52111
Minsu-BAK <77394498+Minsu-BAK@users.noreply.github.com>
2021-01-13 23:30:53 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
-let 몫 = 0
-let 이진수_결과 = 0
-let 입력값 = 0
+let 몫: number = []
+let 리스트: number[] = []
+let 입력값: number = []
 function binary (num: number) {
     몫 = num
+    리스트 = []
     while (몫 != 0) {
-        let 결과 = 0
-        이진수_결과 = 몫 % 2
+        let 결과: number[] = []
+        결과.push(몫 % 2)
         몫 = Math.floor(몫 / 2)
-        basic.showNumber(결과)
         basic.pause(500)
         basic.showLeds(`
             . . . . .
@@ -22,9 +22,9 @@ input.onButtonPressed(Button.A, function () {
     입력값 = 19
     basic.showNumber(입력값)
     binary(입력값)
-    basic.showNumber(0)
 })
 input.onButtonPressed(Button.B, function () {
-    let 리스트 = 0
-    basic.showNumber(리스트)
+    for (let index = 0; index <= 리스트.length; index++) {
+        basic.showNumber(리스트[index])
+    }
 })
